1e37f5fbc447a043577f06da37ff4196dc6ae146
Deuizxc <bacani.johnbernard.tayag@gmail.com>
2026-08-28 17:03:14 +0800

    Initial commit - Starter app setup

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome message ni Vencel, Furton, Bacani, kumbaga VenFurBa</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
