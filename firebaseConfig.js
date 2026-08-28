e1b9c0a21ecf9de1574fa538a6f81c1eb9fbc2a1
Deuizxc <bacani.johnbernard.tayag@gmail.com>
2026-08-28 17:47:37 +0800

    firebasee connection

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHCQKeRN91qvv5hisKWEyfidFyoCIhTro",
+  authDomain: "twoa-aef6e.firebaseapp.com",
+  projectId: "twoa-aef6e",
+  storageBucket: "twoa-aef6e.firebasestorage.app",
+  messagingSenderId: "259465530304",
+  appId: "1:259465530304:web:cb6a3c6a81b38ba9061625"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
